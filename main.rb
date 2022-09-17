514ae7d36a4739cb88f8474585115ee8862b1a80
hipale <hhemmhilb@gmail.com>
2022-09-17 21:25:45 +0200

    Shorten the code

--- main.rb
+++ main.rb
@@ -7,12 +7,11 @@ class Knight
     x = cord_start[0]
     y = cord_start[1]
     if y > 7 || y < 0 || x > 7 || x < 0
-      return 'no'
+      return 
     end
-    puts "#{x} / #{y}"
   
     until cord_start == cord_end
-      moves.push("[#{cord_start[0]}][#{cord_start[1]}]")
+      moves.push([x, y])
       if x - cord_end[0] < y -cord_end[1]
         x > cord_end[0] ? x = x - 2 : x = x + 2
         y > cord_end[1] ? y = y - 1 : y = y + 1
@@ -22,21 +21,29 @@ class Knight
       end
       cord_start = [x, y]
     end
-    moves.push("[#{cord_start[0]}][#{cord_start[1]}]")
+    moves.push([x, y])
+    moves.each { |elm| pp borard = Board.new(elm)}
     return moves
   end
 end
 
 class Board
-  def initialize(array_x = [], array_y = [])
+  def initialize(elm = nil, array_x = [], array_y = [])
     @array_y = array_y
     
-    0.upto(7) do |num|
-      array_x.push(num + 1)
+    0.upto(7) do
+      array_x.push("[-]")
     end
     
-    0.upto(7) do |num|
+    0.upto(7) do
       @array_y.push(array_x)
+    end
+    if elm != nil
+     @array_y.each_with_index do |para, inx|
+      if inx = elm[0]
+        para[elm[1]] = "$"
+      end
+    end
     end
     return @array_y
   end
@@ -47,4 +54,4 @@ end
  pp b
 
  k = Knight.new
-puts k.knight_moves([0, 0], [3, 3])
+print k.knight_moves([1, 1], [5, 6])
