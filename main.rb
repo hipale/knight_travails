f50fcf0786eb48f6fc68ca1f20eb9a7fdc525cc5
hipale <hhemmhilb@gmail.com>
2022-09-18 19:14:11 +0200

    Add graph, of all possible moves

--- main.rb
+++ main.rb
@@ -7,20 +7,17 @@ class Knight
   def find()
   end
 
-  def knight_moves(start_pos, end_pos, count = 0, moves_arr = [start_pos])
-    if moves_arr.one? == false && moves_arr.none? == false
-      return "back"
-    end
+  def knight_moves(start_pos, end_pos, count = 0, moves_arr = [])
+
+    moves_arr.push(start_pos)
+   
     @data = start_pos
     x = @data[0]
     y = @data[1]
-    if x > 7 || x < 0 || y > 7 || y < 0
+    if x > 7 || x < 0 || y > 7 || y < 0 || count == 7
       return "no"
-    elsif start_pos == end_pos
-      puts "as"
-      return moves_arr
     else
-      @moves = []
+     @moves = []
      @moves.push([x + 2, y - 1]) if x < 6 && y > 0
      @moves.push([x - 2, y - 1]) if x > 1 && y > 0
      @moves.push([x + 2, y + 1]) if x < 6 && y < 7
@@ -29,10 +26,16 @@ class Knight
      @moves.push([x - 1, y - 2]) if x > 0 && y > 1
      @moves.push([x + 1, y + 2]) if x < 7 && y < 6
      @moves.push([x + 1, y - 2]) if x < 7 && y > 1
-     @moves.each { |elm| knight_moves(elm, end_pos, count + 1, moves_arr.push([x][y]))}
+     @moves.each  do |elm| 
+      
+      if moves_arr.any? { |para| para == end_pos }
+        return moves_arr
+      end
+      knight_moves(elm, end_pos, count + 1, moves_arr) 
+    end
     end
   end
 end
 
 k = Knight.new
-print k.knight_moves([0, 0], [3, 3])
+print k.knight_moves([0, 0], [4, 4])
