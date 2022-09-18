3f1ccc53e15afa5e5d81b9c77235d1ad0443ce27
hipale <hhemmhilb@gmail.com>
2022-09-18 15:19:32 +0200

    Completly change the code

--- main.rb
+++ main.rb
@@ -1,57 +1,38 @@
 class Knight
+  attr_accessor :data, :moves
   def initialize()
     @piece = "&"
   end
 
-  def knight_moves(cord_start, cord_end, moves = [])
-    x = cord_start[0]
-    y = cord_start[1]
-    if y > 7 || y < 0 || x > 7 || x < 0
-      return 
+  def find()
+  end
+
+  def knight_moves(start_pos, end_pos, count = 0, moves_arr = [start_pos])
+    if moves_arr.one? == false && moves_arr.none? == false
+      return "back"
     end
-  
-    until cord_start == cord_end
-      moves.push([x, y])
-      if x - cord_end[0] < y -cord_end[1]
-        x > cord_end[0] ? x = x - 2 : x = x + 2
-        y > cord_end[1] ? y = y - 1 : y = y + 1
-      else
-        y > cord_end[1] ? y = y - 2 : y = y + 2
-        x > cord_end[0] ? x = x - 1 : x = x + 1
-      end
-      cord_start = [x, y]
+    @data = start_pos
+    x = @data[0]
+    y = @data[1]
+    if x > 7 || x < 0 || y > 7 || y < 0
+      return "no"
+    elsif start_pos == end_pos
+      puts "as"
+      return moves_arr
+    else
+      @moves = []
+     @moves.push([x + 2, y - 1]) if x < 6 && y > 0
+     @moves.push([x - 2, y - 1]) if x > 1 && y > 0
+     @moves.push([x + 2, y + 1]) if x < 6 && y < 7
+     @moves.push([x - 2, y + 1]) if x > 1 && y < 7
+     @moves.push([x - 1, y + 2]) if x > 0 && y < 6
+     @moves.push([x - 1, y - 2]) if x > 0 && y > 1
+     @moves.push([x + 1, y + 2]) if x < 7 && y < 6
+     @moves.push([x + 1, y - 2]) if x < 7 && y > 1
+     @moves.each { |elm| knight_moves(elm, end_pos, count + 1, moves_arr.push([x][y]))}
     end
-    moves.push([x, y])
-    moves.each { |elm| pp borard = Board.new(elm)}
-    return moves
   end
 end
 
-class Board
-  def initialize(elm = nil, array_x = [], array_y = [])
-    @array_y = array_y
-    
-    0.upto(7) do
-      array_x.push("[-]")
-    end
-    
-    0.upto(7) do
-      @array_y.push(array_x)
-    end
-    if elm != nil
-     @array_y.each_with_index do |para, inx|
-      if inx = elm[0]
-        para[elm[1]] = "$"
-      end
-    end
-    end
-    return @array_y
-  end
-
-end
-
- b = Board.new
- pp b
-
- k = Knight.new
-print k.knight_moves([1, 1], [5, 6])
+k = Knight.new
+print k.knight_moves([0, 0], [3, 3])
