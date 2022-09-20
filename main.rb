74af0d453b6fbab17226bab01c23a5f3d42a531d
hipale <hhemmhilb@gmail.com>
2022-09-20 14:25:22 +0200

    Add create_board method, which creates board, with every knight move

--- main.rb
+++ main.rb
@@ -14,22 +14,23 @@ class Board
   def knight_moves(start_pos, end_pos)
     @start_pos = start_pos
     @end_pos = end_pos
-    create_graph(start_pos)
+    new_arr = create_graph(start_pos)
+    create_board(new_arr)
   end
 
   def create_graph(position, parent = nil, count = 0)
-   
-    node = Knight.new(position, parent)
+   node = Knight.new(position, parent)
+
     if node.position == @end_pos
       @moves_arr = [node.position]
       parents = node.parent
       until parents.position == @start_pos
-        @moves_arr.push(parents.position)
+        @moves_arr.unshift(parents.position)
         parents = parents.parent
       end
-      @moves_arr.push(@start_pos)
-      return @moves_arr
-    elsif count == 6 then return
+      @moves_arr.unshift(@start_pos)
+    elsif count == 6 
+      return
     else
     x = position[0]; y = position[1]
     create_graph([x + 2, y - 1], node, count + 1) if x < 6 && y > 0
@@ -43,9 +44,27 @@ class Board
     return @moves_arr
     end
   end
+
+  def create_board(array)
+    y = 0; x = 0
+    until y == 8
+      puts
+      until x == 8
+        if array.any? { |elm| elm[0] == y && elm[1] == x}
+          print "&"
+        else 
+          print "X"
+        end
+        x += 1
+      end
+      y += 1
+      x = 0
+    end
+
+  end
 end
 
 
 
 b = Board.new
-pp b.knight_moves([0, 0], [7, 7])
+puts b.knight_moves([0, 0], [7, 7])
